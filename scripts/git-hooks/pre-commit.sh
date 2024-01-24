1bac62dca44ddea9405579c4a9d4dc4a39ba3703
dave oh <hirakoshinji722@gmail.com>
2024-01-24 16:10:44 +0100

    update pre commit script to include files updated by spotless

--- scripts/git-hooks/pre-commit.sh
+++ scripts/git-hooks/pre-commit.sh
@@ -4,6 +4,9 @@ echo "Running pre -commit checks..."
 JAVA_HOME="/usr/lib/jvm/java-17-openjdk-17.0.9.0.9-3.fc39.x86_64"
 export JAVA_HOME
 
+# add all staged files to an array
+staged_files=$(git diff --staged --name-only)
+
 OUTPUT="/tmp/res"
 ./gradlew lint ktlintFormat ktlintCheck spotlessApply --daemon > ${OUTPUT}
 EXIT_CODE=$?
@@ -14,5 +17,12 @@ if [ ${EXIT_CODE} -ne 0 ]; then
     exit ${EXIT_CODE}
 else
     rm ${OUTPUT}
+    for file in $staged_files
+      do
+      if test -f "$file" ;
+        then
+        git add $file
+      fi
+    done
     echo "Pre Commit Checks Passed -- no problems found"
 fi
