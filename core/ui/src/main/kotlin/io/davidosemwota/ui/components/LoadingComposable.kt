676725c6c2664246b72e1009cf3b7e27810a8daf
dave oh <hirakoshinji722@gmail.com>
2024-12-31 19:07:28 +0100

    Loading composable: Horizontal loading composable ++++

--- core/ui/src/main/kotlin/io/davidosemwota/ui/components/LoadingComposable.kt
+++ core/ui/src/main/kotlin/io/davidosemwota/ui/components/LoadingComposable.kt
@@ -26,12 +26,15 @@ package io.davidosemwota.ui.components
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.width
 import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.LinearProgressIndicator
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import io.davidosemwota.ui.GeneralPreview
 import io.davidosemwota.ui.PreviewComposable
@@ -52,6 +55,32 @@ fun FullScreenLoading(
         CircularProgressIndicator(
             modifier = Modifier.width(32.dp),
             color = MaterialTheme.colorScheme.secondary,
+        )
+    }
+}
+
+@Composable
+fun HorizontalLoadingItem(
+    modifier: Modifier = Modifier,
+    colour: Color = MaterialTheme.colorScheme.secondary,
+    trackColour: Color = MaterialTheme.colorScheme.surfaceVariant,
+) {
+    LinearProgressIndicator(
+        modifier = modifier
+            .fillMaxWidth(),
+        color = colour,
+        trackColor = trackColour,
+    )
+}
+
+@GeneralPreview
+@Composable
+fun PreviewHorizontalLoadingItem(
+    modifier: Modifier = Modifier,
+) {
+    PreviewComposable {
+        HorizontalLoadingItem(
+            modifier = modifier,
         )
     }
 }
